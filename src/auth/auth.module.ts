5f03c902811a4b85f850217cefdd7d4bf4f91f96
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-01-18 18:08:05 +0700

    feat: setup auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from 'dotenv';
+import { jwtConstant } from './constant';
+
+config();
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: jwtConstant.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
